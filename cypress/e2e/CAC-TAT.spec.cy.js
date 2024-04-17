fc750ba4805e25f9516c419c0530b58839dcf298
Julia-Justino <stephany.justino@sptech.school>
2024-04-17 14:26:53 -0300

    documentos do curso

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -3,54 +3,59 @@ beforeEach(() => {
   cy.visit('../src/index.html');
 });
 describe('Central de Atendimento ao Cliente TAT', function () {
+  const SEGUNDOS = 3000;
   it('verifica o título da aplicação', function () {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
   });
   it('Verifica se o formulário foi preenchido corretamente', () => {
-    cy.get('#firstName').type("TESTE");
-    cy.get('#lastName').type("TEstesss");
-    cy.get('#email').type("emailteste@gmail.com");
-    cy.get('#open-text-area').type('TESTETESTETEST', { delay: 0 });
-    cy.contains('button', 'Enviar').click();
-    cy.get('.success').should('be.visible').contains('Mensagem enviada com sucesso.');
-  });
-  it('Verifica se o formulário apresenta erro', () => {
-    cy.get('#firstName').type("TESTE");
-    cy.get('#lastName').type("TEstesss");
-    cy.get('#email').type("emailteste@=gmail.com");
-    cy.get('#open-text-area').type('TESTETESTETEST', { delay: 0 });
-    cy.contains('button', 'Enviar').click();
-    cy.get('.error').should('be.visible').contains('Valide os campos obrigatórios!');
-  });
-  it('Verifica telefone se torna obrigatório mas não é preenchido', () => {
-    cy.get('#firstName').type("TESTE");
-    cy.get('#lastName').type("TEstesss");
-    cy.get('#email').type("emailteste@=gmail.com");
-    cy.get('#open-text-area').type('TESTETESTETEST', { delay: 0 });
-    cy.get('#phone-checkbox').check();
-    cy.contains('button', 'Enviar').click();
-    cy.get('.error').should('be.visible').contains('Valide os campos obrigatórios!');
-  });
-  it('Verifica telefone não permite String', () => {
-    cy.get('#firstName').type("TESTE");
-    cy.get('#lastName').type("TEstesss");
-    cy.get('#email').type("emailteste@=gmail.com");
-    cy.get('#phone').type("abc");
-    cy.get('#open-text-area').type('TESTETESTETEST', { delay: 0 });
-    cy.contains('button', 'Enviar').click();
-    cy.get('.error').should('be.visible').contains('Valide os campos obrigatórios!');
-  });
-  it('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.clock();
     cy.fillMandatoryFieldsAndSubmit();
     cy.get('.success').should('be.visible').contains('Mensagem enviada com sucesso.');
+    cy.tick(SEGUNDOS);
+    cy.get('.success').should('be.not.visible');
+  });
+  it('Verifica se o formulário apresenta erro', () => {
+    cy.clock();
+    cy.preencherCamposSemTelefone();
+    cy.get('#phone-checkbox').check();
+    cy.get('.error').should('be.visible').contains('Valide os campos obrigatórios!');
+    cy.tick(SEGUNDOS);
+    cy.get('.error').should('be.not.visible');
+  });
+  it('Verifica telefone se torna obrigatório mas não é preenchido', () => {
+    cy.clock();
+    cy.preencherCamposSemTelefone();
+    cy.get('#phone-checkbox').check();
+    cy.get('.error').should('be.visible').contains('Valide os campos obrigatórios!');
+    cy.tick(SEGUNDOS);
+    cy.get('.error').should('be.not.visible')
+  });
+  it('Verifica telefone não permite String', () => {
+    cy.clock();
+    cy.preencherCamposSemTelefone()
+    cy.get('#phone').type("abc");
+    cy.get('.error').should('be.visible').contains('Valide os campos obrigatórios!');
+    cy.tick(SEGUNDOS);
+    cy.get('.error').should('be.not.visible');
+  });
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.clock();
+    cy.fillMandatoryFieldsAndSubmit();
+    cy.get('.success').should('be.visible').contains('Mensagem enviada com sucesso.');
+    cy.tick(SEGUNDOS);
+    cy.get('.success').should('be.not.visible');
   });
 
   it('Verifica se o formulário foi apagado corretamente', () => {
-    cy.get('#firstName').type("TESTE")
-    .should('have.value', 'TESTE').clear().should("have.value", '');
+    cy.clock()
+    cy.get('#firstName').type("TESTE").should('have.value', 'TESTE').clear().should("have.value", '');
     cy.get('#lastName').type("TEstesss").should('have.value', 'TEstesss').clear().should("have.value", '');
     cy.get('#email').type("emailteste@gmail.com").should('have.value', 'emailteste@gmail.com').clear().should("have.value", '');
     cy.get('#phone').type("11111111111").should('have.value', '11111111111').clear().should("have.value", '');
+    cy.contains('button', 'Enviar').click();
+    cy.get('.error').should('be.visible').contains('Valide os campos obrigatórios!');
+    cy.tick(SEGUNDOS);
+    cy.get('.error').should('be.not.visible');
   });
   it('Valida exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
     cy.contains('button', 'Enviar').click();
@@ -122,4 +127,60 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
     cy.get('#privacy a').invoke('removeAttr', 'target').click();
   });
+
+  it('Valida exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.clock();
+    cy.fillMandatoryFieldsAndSubmit();
+    cy.get('.success').should('be.visible');
+    cy.tick(SEGUNDOS);
+    cy.get('.success').should('not.be.visible');
+  });
+
+  Cypress._.times(5, ()=>{
+    it('envia o formuário com sucesso usando um comando customizado', () => {
+      cy.clock();
+      cy.fillMandatoryFieldsAndSubmit();
+      cy.get('.success').should('be.visible').contains('Mensagem enviada com sucesso.');
+      cy.tick(SEGUNDOS);
+      cy.get('.success').should('be.not.visible');
+    });
+  });
+
+  it('simulates sending a CTRL+V command to paste a long text on a textarea field', () => {
+    const longText = Cypress._.repeat('0123456789', 20)
+    cy.get('#open-text-area')
+      .invoke('val', longText)
+      .should('have.value', longText)
+  });
+
+  it('busca elementos corretamente', () => {
+   cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html').
+   should(function(response){
+    const {status, statusText, body} = response
+    expect(status).to.equal(200)
+    expect(statusText).to.equal('OK')
+    expect(body).to.include('CAC TAT')
+   })
+  });
+
+  it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', () => {
+    cy.get('.success')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.')
+      .invoke('hide')
+      .should('not.be.visible')
+    cy.get('.error')
+      .should('not.be.visible')
+      .invoke('show')
+      .should('be.visible')
+      .and('contain', 'Valide os campos obrigatórios!')
+      .invoke('hide')
+      .should('not.be.visible')
+  })
+
+  it.only('Acha Gato', () => {
+    cy.get('#cat').should('be.not.visible').invoke('show');
+  }); 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,12 +24,18 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
-    cy.get('#firstName').type("TESTE");
-    cy.get('#lastName').type("TEstesss");
-    cy.get('#email').type("emailteste@gmail.com");
-    cy.get('#phone').type("111111111111");
-    cy.get('#open-text-area').type('TESTETESTETEST');
-    cy.contains('button', 'Enviar').click();
+    cy.get('#firstName').type("TESTE"), {delay: 0};
+    cy.get('#lastName').type("TEstesss"), {delay: 0};
+    cy.get('#email').type("emailteste@gmail.com"), {delay: 0};
+    cy.get('#phone').type("111111111111"), {delay: 0};
+    cy.get('#open-text-area').type('TESTETESTETEST'), {delay: 0};
+    cy.contains('button', 'Enviar').click(), {delay: 0};
+});
+Cypress.Commands.add('preencherCamposSemTelefone', function(){
+  cy.get('#firstName').type("TESTE"), {delay: 0};
+  cy.get('#email').type("emailteste@gmail.com"), {delay: 0};
+  cy.get('#open-text-area').type('TESTETESTETEST'), {delay: 0};
+  cy.contains('button', 'Enviar').click(), {delay: 0};
 });
 Cypress.Commands.add('checkGeral',  function(){  
     cy.get('input[type="radio"]').should('have.length', 3)
